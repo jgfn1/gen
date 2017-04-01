1acb6c410b3238561cdd81034b25e973c2f89ca2
Jose Gerson Fialho Neto <jgfn@ug2c15.windows.cin.ufpe.br>
2017-04-01 10:14:55 -0300

    Fully working Binary Search Algorithm.

--- binary_search.c
+++ binary_search.c
@@ -1,25 +1,49 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "math.h"
 
-int binary_search(int* vector, int vec_size, int x);
+int binary_search(int* vector, int vec_size, int target);
 int main()
 {
+	int vec_size;
+	int* vector;
+	int target;
+	int i;
 
+	printf("Insert the vector size: \n");
+	scanf("%d", &vec_size);
+
+	vector = (int*) calloc(vec_size, sizeof(int));
+
+	printf("Insert the vector: \n");
+	for(i = 0; i < vec_size; ++i)
+		scanf("%d", &vector[i]);
+
+	printf("Insert the target value: \n");
+	scanf("%d", &target);
+
+	int position = binary_search(vector, vec_size, target);
+	if(position == -1)
+		printf("The target is not in the vector.\n");
+	else
+		printf("The target is in the vector[%d] position.\n", position);
+
+	free(vector);
 	return 0;
 }
 
-int binary_search(int* vector, int vec_size, int x)
+int binary_search(int* vector, int vec_size, int target)
 {
 	int left = 0;
 	int right = vec_size - 1;
 	int index;
 	do
 	{
-		index = floor((left + right)/2);
-		if(x == vector[index])
+		index = /*floor*/((left + right)/2);
+		if(target == vector[index])
 			return index;
 
-		else if(x < vector[index])
+		else if(target < vector[index])
 			right = index - 1;
 		
 		else
